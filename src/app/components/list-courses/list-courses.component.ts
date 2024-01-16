a911828bf16a1a47d59706bca1018abddd5f711f
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2024-01-16 12:10:06 +0100

    second day

---
+++ src/app/components/list-courses/list-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-courses.component.html',
+  styleUrl: './list-courses.component.css'
+})
+export class ListCoursesComponent {
+
+}
--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-todo',
@@ -11,12 +12,44 @@ import { FormsModule } from '@angular/forms';
 export class TodoComponent {
 
   title = "";
-  todos = ['learn angular', 'javascript', 'typescript for oop']
+  todos = ['angular', 'javascript', 'angular']
 
   addTodo() {
     // this.todos.unshift(this.title)
 
     this.todos = [this.title, ...this.todos]
     this.title = ""
+  }
+
+  deleteTodo(i: number) {
+
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "delete it",
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.todos.splice(i, 1)
+
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+          timer: 3000,
+          timerProgressBar: true
+        });
+      }
+    });
+
+  }
+
+
+  editTodo(todo: string) {
+    this.title = todo
   }
 }
